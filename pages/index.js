385672e4873c9675cf827caedda6cd11b044d123
ktiakib <ktiakib@gmail.com>
2022-02-03 22:01:22 +0600

    code

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,11 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="">
-      
-
-      <main className="">
+    <div className=''>
+      <main className=''>
         <h1>Hello</h1>
       </main>
-
-      <footer className=""></footer>
+      <footer className=''></footer>
     </div>
   );
 }
